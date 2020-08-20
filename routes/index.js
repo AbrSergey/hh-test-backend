1273e0b96d995c0c8ecc9075c28d758539de9605
Sergey <sergey.abramov996@gmail.com>
2020-08-20 19:15:58 +0700

    change path routes

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,9 @@ router.get('/users', users.get);
 router.post('/users', validate(users.schema), users.post);
 
 // routes for mvp-crash-course
-router.get('/resume/:guid/social_links', resume.socalLinks.get);
-router.get('/resume/:guid/general_information', resume.generalInformation.get);
-router.get('/resume/:guid/skills', resume.skills.get);
-router.get('/resume/:guid/education', resume.education.get);
+router.get('/resume/social_links', resume.socalLinks.get);
+router.get('/resume/general_information', resume.generalInformation.get);
+router.get('/resume/skills', resume.skills.get);
+router.get('/resume/education', resume.education.get);
 
 module.exports = router;
