9e25a024bb39fba8b214c5e2eac60510bedcbc06
Sergey <sergey.abramov996@gmail.com>
2020-08-20 19:19:35 +0700

    fi

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/users', users.get);
 router.post('/users', validate(users.schema), users.post);
 
 // routes for mvp-crash-course
-router.get('/resume/social_links', resume.socalLinks.get);
+router.get('/resume/social_links', resume.socialLinks.get);
 router.get('/resume/general_information', resume.generalInformation.get);
 router.get('/resume/skills', resume.skills.get);
 router.get('/resume/education', resume.education.get);
